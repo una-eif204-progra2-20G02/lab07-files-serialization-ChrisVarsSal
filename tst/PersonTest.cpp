992b1e2a0df86a8e7c9a7a52069a84ec49002b0a
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2020-10-07 14:34:41 -0600

    Creación del main para el test

--- tst/PersonTest.cpp
+++ tst/PersonTest.cpp
@@ -2,3 +2,15 @@
 // Created by Chris on 10/6/2020.
 //
 
+#include <Person.h>
+#include "gtest/gtest.h"
+
+TEST(ProductoPerecederoTestSuite, CheckToString){
+ProductoPerecedero producto;
+
+producto.setNombre("Manzana");
+producto.setCantidad(2);
+producto.setPrecio(800);
+
+EXPECT_EQ(producto.toString(),"-- Producto perecedero --\nNombre: Manzana\nCantidad: 2\nPrecio: 800\n\n");
+}
