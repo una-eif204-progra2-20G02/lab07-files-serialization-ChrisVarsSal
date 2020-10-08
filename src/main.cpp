aa7a2115689f8d9ddee67f56693eac094e00a623
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2020-10-08 17:53:39 -0600

    Creación del main de la aplicación 1

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Chris on 10/8/2020.
+//
+
+#include "Binario.h"
+#include "Texto.h"
+#include "Person.h"
+
+int main(){
+    Person* person1 = new Person("Chris",1111,19);
+    Person* person2 = new Person("Alexis",2222,20);
+    Person* person3 = new Person("Sofia",3333,21);
+    Person* person4 = new Person("Mariana",4444,22);
+    Person* person5 = new Person("Mau",5555,23);
+    Person* person6 = new Person("Manuel",6666,24);
+    Person* person7 = new Person("Michi",7777,25);
+    Person* person8 = new Person("Solano",8888,26);
+
+    Texto texto;
+    /*Escritura*/
+    texto.escribir(person1);
+    texto.escribir(person2);
+
+    /*Lectura*/
+    texto.leer(person5);
+    texto.leer(person6);
+
+    Binario binario;
+    /*Escritura*/
+    binario.escribir(person3);
+    binario.escribir(person4);
+
+    /*Lectura*/
+    binario.leer(person7);
+    binario.leer(person8);
+
+    return 0;
+}
